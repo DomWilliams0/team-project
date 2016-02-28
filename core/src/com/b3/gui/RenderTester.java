6e22edb22f17cf9f27e1405168de5863ea131922
Nishanth Ganatra <nbg481@bcddc6f8-a9be-49cb-8389-49b67bae5dd7>
2016-02-28 21:06:46 +0000

    Bug fix to rendertester

--- core/src/com/b3/gui/RenderTester.java
+++ core/src/com/b3/gui/RenderTester.java
@@ -22,7 +22,6 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.sun.org.apache.xpath.internal.operations.String;
 
 public class RenderTester {
 
