cc5b9710d046acf7c2b9ff3cd42a1e2909766383
Dom Williams <me@domwillia.ms>
2016-03-04 14:09:29 +0000

    Added 3 flocking steering behaviours

---
+++ core/src/com/b3/entity/ai/SteeringAlignment.java
@@ -0,0 +1,42 @@
+package com.b3.entity.ai;
+
+import com.b3.entity.component.PhysicsComponent;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.List;
+
+public class SteeringAlignment extends Steering {
+
+	private static Vector2 tmpVector = new Vector2();
+	private static final int ALIGNMENT_DISTANCE_SQRD = 5 * 5;
+
+	private List<PhysicsComponent> entities;
+
+	public SteeringAlignment(PhysicsComponent entity, List<PhysicsComponent> entities) {
+		super(entity);
+		this.entities = entities;
+
+		// remove self
+		entities.remove(entity);
+	}
+
+	@Override
+	public void tick(Vector2 steeringOut) {
+		steeringOut.setZero();
+		int count = 0;
+
+		for (PhysicsComponent e : entities) {
+			tmpVector.set(e.getPosition());
+			if (tmpVector.sub(entity.getPosition()).len2() < ALIGNMENT_DISTANCE_SQRD) {
+				steeringOut.add(e.body.getLinearVelocity());
+				count += 1;
+			}
+		}
+
+		if (count != 0)
+			steeringOut
+					.scl(1f / count)
+					.nor();
+
+	}
+}
