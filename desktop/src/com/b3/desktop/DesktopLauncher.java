f0f6a84ad06448b1428f86971a55215af7dfab81
Nishanth Ganatra <nbg481@bcddc6f8-a9be-49cb-8389-49b67bae5dd7>
2016-03-07 15:39:11 +0000

    Added title

--- desktop/src/com/b3/desktop/DesktopLauncher.java
+++ desktop/src/com/b3/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.b3.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.b3.MainGame;
@@ -9,6 +10,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1024;
 		config.height = 768;
+		config.title = "Cop Chase";
 		new LwjglApplication(new MainGame(), config);
 	}
 }
