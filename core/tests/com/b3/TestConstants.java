1678d110e456954cdbd3c467c888354f99d4d792
Dom Williams <me@domwillia.ms>
2016-03-24 02:49:10 +0000

    I missed a SINGLE javadoc, grah!

--- core/tests/com/b3/TestConstants.java
+++ core/tests/com/b3/TestConstants.java
@@ -1,10 +1,16 @@
 package com.b3;
 
 /**
+ * Certain constants that are to shared across all tests
+ *
  * @author dxw405
  */
 public class TestConstants {
 	public static final float EQ_THRESHOLD = 0.001f;
 	public static final String REFERENCE_CONFIG = "tests/resources/reference.yml";
 	public static final String USER_CONFIG = "tests/resources/userconfig.yml";
+
+	private TestConstants() {
+		// no instantiation4u
+	}
 }
